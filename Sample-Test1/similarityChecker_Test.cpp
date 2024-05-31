52c34babcbd6f61ede08505bbd5550c66caf31fa
giyeong.kim <kimki1117@gmail.com>
2024-05-31 10:25:54 +0900

    Add alpha method.

--- Sample-Test1/similarityChecker_Test.cpp
+++ Sample-Test1/similarityChecker_Test.cpp
@@ -1,7 +1,51 @@
 #include "pch.h"
 #include "../StringGame/similarityChecker.cpp"
 
-TEST(TestCaseName, TestName) {
-    EXPECT_EQ(1, 1);
-    EXPECT_TRUE(true);
+class SimilarityCheckerFixture : public testing::Test {
+public:
+	SimilarityChecker checker;
+
+	void checkScore(string str1, string str2, int expectedScroe) {
+		int actualScore = checker.similarityCheck(str1, str2);
+		EXPECT_EQ(expectedScroe, actualScore);
+	}
+
+	void assertIllegalArgument(string str1, string str2) {
+		try {
+			int actualScore = checker.similarityCheck(str1, str2);
+			FAIL();
+		}
+		catch (std::exception e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+};
+
+TEST_F(SimilarityCheckerFixture, SameStringChecker) {
+	checkScore("ACCTET", "ACCTET", 100);
+}
+
+
+TEST_F(SimilarityCheckerFixture, EverySameAlphaBet) {
+	checkScore("TVB", "VBT", 40);
+}
+
+TEST_F(SimilarityCheckerFixture, EveryDifferentCharacter) {
+	checkScore("ACF", "DGN", 0);
+}
+
+TEST_F(SimilarityCheckerFixture, SomeSameAlphaBet) {
+	checkScore("ACCTTT", "TDC", 20);
+}
+
+TEST_F(SimilarityCheckerFixture, SomeSameAlphaBet2) {
+	checkScore("ACCTTT", "TDCT", 20);
+}
+
+TEST_F(SimilarityCheckerFixture, InvalidAlphaBet1) {
+	assertIllegalArgument("ACCTTT", "cDC");
+}
+
+TEST_F(SimilarityCheckerFixture, InvalidAlphaBet2) {
+	assertIllegalArgument("cCTTT", "CVE");
 }
--- StringGame/similarityChecker.cpp
+++ StringGame/similarityChecker.cpp
@@ -1,3 +1,61 @@
-class SimilarityChecker {
+#include <string>
+#include <stdexcept>
+using namespace std;
 
+class SimilarityChecker {
+public:
+	int similarityCheck(string str1, string str2) {
+		if (str1 == str2)
+			return 100;
+
+		return calculateSameAlphabetScore(str1, str2);
+	}
+
+	int calculateSameAlphabetScore(std::string& str1, std::string& str2)
+	{
+		// 0 : is not exist, 1 : exist at str1, 2 : exist at str2
+		int isCheckedCharacter[26] = { 0, };
+		int sameCnt = 0;
+		int totalCnt = 0;
+
+		checkFirstStr(str1, isCheckedCharacter, totalCnt);
+		checkSencondStr(str2, isCheckedCharacter, totalCnt, sameCnt);
+
+		return (sameCnt * 40) / totalCnt;
+	}
+
+	void checkSencondStr(std::string& str2, int  isCheckedCharacter[26], int& totalCnt, int& sameCnt)
+	{
+		for (const char& ch : str2) {
+			int index = ch - 'A';
+			checkValidAlphabet(index);
+
+			if (isCheckedCharacter[index] == 2)
+				continue;
+
+			if (isCheckedCharacter[index] == 0) totalCnt++;
+			else	sameCnt++;
+
+			isCheckedCharacter[index] = 2;
+		}
+	}
+
+	void checkFirstStr(std::string& str1, int  isCheckedCharacter[26], int& totalCnt)
+	{
+		for (const char& ch : str1) {
+			int index = ch - 'A';
+			checkValidAlphabet(index);
+			if (isCheckedCharacter[index] == 0) {
+				isCheckedCharacter[index]++;
+				totalCnt++;
+			}
+		}
+	}
+
+	void checkValidAlphabet(int index)
+	{
+		if (index < 0 || index >25)
+			throw std::invalid_argument(" 입력 값은 Alphabet 대문자만 가능합니다. ");
+	}
 };
+
